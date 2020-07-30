26a540c7a300da4136fc3874705dfd19676deecc
zmahfuz061318 <str053mgr@gmail.com>
2020-07-30 17:59:34 -0400

    Work on progress

--- dad-jokes/src/App.js
+++ dad-jokes/src/App.js
@@ -1,27 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import axios from "axios"
+
+
+
 
 function App() {
+
+  const [dadJoke, updateDadJoke] = useState({})
+
+  const callApi = async () => {
+    const result = await axios({
+      baseURL: 'https://icanhazdadjoke.com',
+      headers: {
+        Accept: "application/json"
+      }
+
+    })
+    updateDadJoke(result.data)
+  }
+
+  useEffect(() => {
+    callApi()
+  }, [])
+  function randomJokes() {
+  
+    
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>
+        <h1>{dadJoke.joke}</h1>
+        <button onClick={() => updateDadJoke(dadJoke)}>New Joke</button>
+      </div>
+    </>
+
+  )
 }
 
-export default App;
-// current file
+export default App
